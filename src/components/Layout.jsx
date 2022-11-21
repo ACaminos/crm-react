18a440e6957878f90375dad5bdd0b1fa18b8e9e2
Ariel <caminosariel@gmail.com>
2022-11-21 19:19:40 -0300

    add tailwind, react-router-dom and define something routes

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from 'react-router-dom'
+function Layout() {
+  return (
+    <>
+        <h1 className="text-6xl font-bold">CRM - React</h1>
+
+        <Outlet />
+    </>
+  )
+}
+
+export default Layout
